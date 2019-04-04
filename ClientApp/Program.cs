ea2ca7f611b842b68a1850f4596d1613389bf86a
muradheydarov <muradheyderov@gmail.com>
2019-04-05 03:01:58 +0400

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static string host = "127.0.0.1";
+        static int port = 8051;
+        static void Main(string[] args)
+        {            
+            TcpClient client = default(TcpClient);
+
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+            
+            try
+            {
+                client = new TcpClient(host,port);
+                NetworkStream stream = client.GetStream();
+                string message = string.Empty;
+
+                while (message != "close")
+                {
+                    Console.Write("Message: ");
+                    message = Console.ReadLine();
+
+                    string data = $"{username} : {message}";
+                    byte[] dataArr = Encoding.UTF8.GetBytes(data);
+
+                    stream.Write(dataArr, 0, dataArr.Length);
+
+                    dataArr = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(dataArr, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(dataArr, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    Console.WriteLine($"From server {builder.ToString()}");                        
+                }
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8051;
+
+            try
+            {
+                IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+                TcpListener listener = new TcpListener(iPAddress,port);
+
+                listener.Start();
+                Console.WriteLine("Waiting for connection...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = listener.AcceptTcpClient();
+                    Console.WriteLine("Client Connected");
+
+                    Client client = new Client(tcpClient);
+
+                    Thread thread = new Thread(new ThreadStart(client.Process));
+                    thread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    class Client
+    {
+        public TcpClient client;
+
+        public Client(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+
+        public void Process()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[64];
+                string message = string.Empty;
+
+                while (message.ToLower() != "close")
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine(message);
+                    message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
+
+                    byte[] responseData = Encoding.UTF8.GetBytes(message);
+                    stream.Write(responseData, 0, responseData.Length);
+                }
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
